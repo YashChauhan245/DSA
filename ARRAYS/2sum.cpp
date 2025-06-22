f4fbbc23f4e85df7b49073d26c1673a5d3981084
Yash Chauhan <yashchau.work@gmail.com>
2025-06-22 14:07:50 +0530

    Create 2sum.cpp

---
+++ ARRAYS/2sum.cpp
@@ -0,0 +1,36 @@
+// Leetcode-https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+// BRUTE FORCE:
+        // vector<int>ans;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+                    
+        //         }
+        //     }
+        // } 
+        // return ans;
+
+
+// OPTIMISED USING HASH
+
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int ans=target-nums[i];
+            if(mp.find(ans)!=mp.end()){
+                return{mp[ans],i};
+            }
+            mp[nums[i]]=i;
+
+        }
+        return {};
+
+    }
+};
