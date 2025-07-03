0c3b9f91f6a322abe4d7c02b2b277aef71d46604
Yash Chauhan <yashchau.work@gmail.com>
2025-07-03 16:56:42 +0530

    Create longest common prefix.cpp

---
+++ STRINGS/longest common prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string first=strs.front();
+        string last=strs.back();
+        int minlength=min(first.size(),last.size());
+        int i=0;
+        while(i<minlength){
+            if (first[i]==last[i]){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+    return first.substr(0,i);
+    }
+};
