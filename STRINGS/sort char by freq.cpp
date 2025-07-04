a38786ff0b7a0ed0ffce595a751201fe668f7218
Yash Chauhan <yashchau.work@gmail.com>
2025-07-04 13:12:12 +0530

    Create sort char by freq.cpp

---
+++ STRINGS/sort char by freq.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<pair<char,int>>v;
+        int freqtable[256]={0};
+        for(int i=0;i<s.length();i++){
+            freqtable[s[i]]++;
+        }  
+        for(int i=0;i<256;i++){
+            if(freqtable[i]>0){
+                v.push_back({(char)i,freqtable[i]});
+            }
+        }
+        sort(v.begin(), v.end(), [](const auto& a, const auto& b){
+            return a.second > b.second;
+        });
+        string result ="";
+        for(auto it=v.begin();it!=v.end();it++){
+            result.append(it->second,it->first);
+        }
+        return result;
+    }
+};
