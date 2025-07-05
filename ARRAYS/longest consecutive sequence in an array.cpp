486856b014b693bd218e9f1c3d724ccf561e347c
Yash Chauhan <yashchau.work@gmail.com>
2025-07-05 16:51:43 +0530

    Update longest consecutive sequence in an array.cpp

--- ARRAYS/longest consecutive sequence in an array.cpp
+++ ARRAYS/longest consecutive sequence in an array.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-         int n = nums.size();
+    int n = nums.size();
     if (n == 0) return 0;
 
     int longest = 1;
@@ -12,16 +12,12 @@ public:
     for (int i = 0; i < n; i++) {
         st.insert(nums[i]);
     }
-
-    //Find the longest sequence:
-    for (auto it : st) {
-        //if 'it' is a starting number:
-        if (st.find(it - 1) == st.end()) {
-            //find consecutive numbers:
+    for (auto it : st) {    //iterating  set and checking is x-1 is present
+        if (st.find(it - 1) == st.end()) { //if not present  i.e its the starting pt.
             int cnt = 1;
             int x = it;
-            while (st.find(x + 1) != st.end()) {
-                x = x + 1;
+            while (st.find(x + 1) != st.end()) {  //now checking for x+1 ...(1,2,3...)
+                x = x + 1;      //updating, so that loop runs till the last longest
                 cnt = cnt + 1;
             }
             longest = max(longest, cnt);
