89b0b7bfddc5f13fd58514d8f51a1a72778b80a3
Yash Chauhan <yashchau.work@gmail.com>
2025-07-06 17:37:01 +0530

    Update atoi.cpp

--- RECURSION/atoi.cpp
+++ RECURSION/atoi.cpp
@@ -1,1 +1,38 @@
+https://leetcode.com/problems/string-to-integer-atoi/
 
+class Solution {
+public:
+
+    int helper(const string &s,int i,long long result,int sign){
+        // base condn
+        if(i>=s.size() || !isdigit(s[i])){
+            return result*sign;
+        }
+        // converting func
+        result=result*10+(s[i]-'0');
+        if(sign==1 && result>=INT_MAX){
+            return INT_MAX;
+        }
+        if(sign==-1 && -result<=INT_MIN){
+            return INT_MIN;
+        }
+        return helper(s,i+1,result,sign);
+
+    }
+    int myAtoi(string s){
+        // whitespace ignore
+        int i=0;
+        while(i<s.size() && s[i]==' '){
+            i++;
+        }
+        // sign store
+        int sign =1;
+        if(i<s.size()&& (s[i]=='+' || s[i]=='-')){
+            if (s[i]=='-'){
+                sign=-1;
+            }
+            i++;
+        }
+        return helper(s,i,0,sign);//0-->result
+    }
+};
