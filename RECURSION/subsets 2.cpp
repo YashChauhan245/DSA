d232aad62961d0473a27044a230700940084c984
Yash Chauhan <yashchau.work@gmail.com>
2025-07-07 19:45:42 +0530

    Update subsets 2.cpp

--- RECURSION/subsets 2.cpp
+++ RECURSION/subsets 2.cpp
@@ -1,2 +1,28 @@
 https://leetcode.com/problems/subsets-ii/description/
 
+
+class Solution {
+public:
+    vector<vector<int>>result;
+    void solve(vector<int>& nums,vector<int>temp,int i){
+        // no proper base case as want all subsets including empty set
+        result.push_back(temp);
+        for(int j=i;j<nums.size();j++){
+            if(j>i && nums[j]==nums[j-1]){
+                continue;
+            }
+            temp.push_back(nums[j]);
+            solve(nums,temp,j+1);
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>temp;
+        solve(nums,temp,0);
+        return result;
+
+    }
+};
+
