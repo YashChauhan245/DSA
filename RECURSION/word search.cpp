9580d34b02d7840fbae6e8f50e42102c4bff142b
Yash Chauhan <yashchau.work@gmail.com>
2025-07-08 21:05:27 +0530

    Create word search.cpp

---
+++ RECURSION/word search.cpp
@@ -0,0 +1,47 @@
+https://leetcode.com/problems/word-search/
+
+class Solution {
+public:
+    int l, m, n;
+    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
+        if (idx >= l)
+            return true;
+
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '$';
+
+        for (int d = 0; d < 4; d++) { 
+            int i_ = i + directions[d][0];
+            int j_ = j + directions[d][1];
+            if (find(board, i_, j_, word, idx + 1))
+                return true;
+        }
+
+        board[i][j] = temp;
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        m = board.size();
+        n = board[0].size();
+        l = word.length();
+        
+        if (m * n < l)
+            return false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0] && find(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
