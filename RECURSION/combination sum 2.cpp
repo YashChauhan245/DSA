5e8400e129a572a78a3a18e022cea19bfa7526b1
Yash Chauhan <yashchau.work@gmail.com>
2025-07-10 15:01:45 +0530

    Update combination sum 2.cpp

--- RECURSION/combination sum 2.cpp
+++ RECURSION/combination sum 2.cpp
@@ -2,37 +2,29 @@ https://leetcode.com/problems/combination-sum-ii/submissions/1689397794/
 
 class Solution {
 public:
-    vector<vector<int>> result;
-
-    void solve(vector<int>& candidates, int target, vector<int>& temp, int start) {
-        if (target == 0) {
+    vector<vector<int>>result;
+    void solve(vector<int>& candidates, int target,vector<int>temp,int idx){
+        if(target==0){
             result.push_back(temp);
-            return;
+            return ;
         }
-
-        for (int i = start; i < candidates.size(); i++) {
-            // Skip duplicates at the same recursion level
-            if (i > start && candidates[i] == candidates[i - 1])
+        for(int i=idx;i<candidates.size();i++){
+            if(i>idx && candidates[i]==candidates[i-1]){
                 continue;
-
-            // If current candidate is greater than remaining target, break (since array is sorted)
-            if (candidates[i] > target)
+            }
+            if(candidates[i]>target){
                 break;
-
-            // Include this candidate
+            }
             temp.push_back(candidates[i]);
-            solve(candidates, target - candidates[i], temp, i + 1);
-            
-            // Backtrack
+            solve(candidates,target-candidates[i],temp,i+1);
             temp.pop_back();
         }
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end()); // Sort to handle duplicates easily
-        vector<int> temp;
-        solve(candidates, target, temp, 0);
+        sort(candidates.begin(),candidates.end());
+        vector<int>temp;
+        solve(candidates,target,temp,0);
         return result;
+        
     }
 };
-
