8325a63a357ea2b97d5b07e9489b0510ba8c2175
Yash Chauhan <yashchau.work@gmail.com>
2025-07-12 15:36:53 +0530

    Update detect cycle.cpp

--- LINKED LIST/detect cycle.cpp
+++ LINKED LIST/detect cycle.cpp
@@ -1,19 +1,19 @@
 https://leetcode.com/problems/linked-list-cycle/submissions/1695008877/
 
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if (head == nullptr || head->next == nullptr) return false;
 
         ListNode* slow = head;       
-        ListNode* fast = head->next;   
+        ListNode* fast = head;   //not head bcz ist condn me hi true de dega 
 
         while (fast != nullptr  && fast->next != nullptr) {
-            if (slow == fast)          
-                return true;
             slow = slow->next;        
-            fast = fast->next->next;   
+            fast = fast->next->next;
+            if (slow == fast){
+                return true; 
+            }
         }
         return false;
     }
