12c124d14acafad1a26aab3b222d1bb3adf9d1cc
Yash Chauhan <yashchau.work@gmail.com>
2025-07-13 19:01:24 +0530

    Update reverse k nodes.cpp

--- LINKED LIST/reverse k nodes.cpp
+++ LINKED LIST/reverse k nodes.cpp
@@ -1,1 +1,59 @@
 https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+
+class Solution {
+public:
+    ListNode* getKthNode(ListNode* temp, int k) {
+        k -= 1;
+        while (temp != NULL && k > 0) {
+            k--;
+            temp = temp->next;
+        }
+        return temp;
+    }
+
+    ListNode* reverseLinkedList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        ListNode* prevLast = NULL;
+
+        while (temp != NULL) {
+            ListNode* kThNode = getKthNode(temp, k);
+            if (kThNode == NULL) {
+                if (prevLast) {
+                    prevLast->next = temp;
+                }
+                break;
+            }
+
+            ListNode* nextGroup = kThNode->next;
+            kThNode->next = NULL;
+
+            ListNode* reversedHead = reverseLinkedList(temp);
+
+            if (temp == head) {
+                head = reversedHead;
+            } else {
+                prevLast->next = reversedHead;
+            }
+
+            prevLast = temp;
+            temp = nextGroup;
+        }
+
+        return head;
+    }
+};
+
+
