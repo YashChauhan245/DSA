c8bd01d86db9d1f9946b15e89097752ee110bfcf
Yash Chauhan <yashchau.work@gmail.com>
2025-07-13 22:27:05 +0530

    Create deep copy.cpp

---
+++ LINKED LIST/deep copy.cpp
@@ -0,0 +1,40 @@
+https://leetcode.com/problems/copy-list-with-random-pointer/submissions/1696609965/
+
+APPROACH-1) USING MAP
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head)
+            return NULL;
+        map<Node*, Node*> mp;
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* newHead = NULL;
+        while(curr) {
+            Node* temp = new Node(curr->val);
+            mp[curr] = temp;
+            if(newHead == NULL) {
+                newHead = temp;
+                prev = newHead;
+            } else {
+                prev->next = temp;
+                prev = temp;
+            }
+            curr = curr->next;
+        }
+        
+        curr = head;
+        Node* newCurr = newHead;
+        while(curr) {
+            if(curr->random == NULL) {
+                newCurr->random = NULL;
+            } else {
+                newCurr->random = mp[curr->random];
+            }
+            
+            newCurr = newCurr->next;
+            curr = curr->next;
+        }
+        return newHead;
+    }
+};
