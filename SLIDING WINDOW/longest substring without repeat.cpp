31fbd62365aec9bff53d26ee702211fe1d13409a
Yash Chauhan <yashchau.work@gmail.com>
2025-07-19 16:10:31 +0530

    Update longest substring without repeat.cpp

--- SLIDING WINDOW/longest substring without repeat.cpp
+++ SLIDING WINDOW/longest substring without repeat.cpp
@@ -1,5 +1,6 @@
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+BRTUE:
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -15,6 +16,42 @@ public:
                 maxlen=max(maxlen,length);
 
             }
+        }
+        return maxlen;
+        
+    }
+};
+
+
+
+
+APPROACH-2)OPTIMAL
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0;
+        int r=0;
+        int leng=0;
+        int maxlen=0;
+        int n=s.size();
+        int hash[256];
+        for (int i = 0; i < 256; ++i) {
+            hash[i] = -1;
+        }
+        while(r<n){
+            // if r is in the map
+            if(hash[s[r]]!=-1){
+                // r is in the same string 
+                if(hash[s[r]]>=l){
+                    // l->update
+                    l=hash[s[r]]+1;
+                }
+            }
+            leng=r-l+1;
+            maxlen=max(maxlen,leng);
+            hash[s[r]]=r;
+            r++;
         }
         return maxlen;
         
