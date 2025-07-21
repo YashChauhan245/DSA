a55d6a4676972afb4f4f7b8a7dee2eaac07e40d2
Yash Chauhan <yashchau.work@gmail.com>
2025-07-21 18:38:07 +0530

    Update kth largest element in an array.cpp

--- HEAPS/kth largest element in an array.cpp
+++ HEAPS/kth largest element in an array.cpp
@@ -1,1 +1,15 @@
 https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>minhp;
+        for(int i=0;i<nums.size();i++){
+            minhp.push(nums[i]);
+            if(minhp.size()>k){
+                minhp.pop();
+            }
+        }
+       return minhp.top(); 
+    }
+};
