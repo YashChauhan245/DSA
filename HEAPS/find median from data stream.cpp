63a8a3701882edf8a2e74309d0de42fe14afb640
Yash Chauhan <yashchau.work@gmail.com>
2025-07-23 01:52:46 +0530

    Update find median from data stream.cpp

--- HEAPS/find median from data stream.cpp
+++ HEAPS/find median from data stream.cpp
@@ -43,37 +43,35 @@ public:
     priority_queue<int> left_max_heap; //max heap
     priority_queue<int, vector<int>, greater<int>> right_min_heap; //min heap
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
+        // left heap is empty or arr[i]<st.top -->push in left else in right
         if(left_max_heap.empty() || num < left_max_heap.top()) {
             left_max_heap.push(num);
         } else {
             right_min_heap.push(num);
         }
         
-        
         //always maintain left_max_heap size one greater than rigfht_min_heap size
         //ya fir, dono ka size equal ho
-        
+        // **if diff is greater than 1 to left se right dalo
         if(abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1) {
             right_min_heap.push(left_max_heap.top());
             left_max_heap.pop();
+    // else right se left push 
         } else if(left_max_heap.size() < right_min_heap.size()) {
             left_max_heap.push(right_min_heap.top());
             right_min_heap.pop();
-        }
-        
+        }   
     }
-    
+
+
     double findMedian() {
         if(left_max_heap.size() == right_min_heap.size()) {
-            // matlab even number of elements hue honge
-            
+            // matlab even number of elements hue honge  
             return (double)(left_max_heap.top()+right_min_heap.top())/2;
-        }
-        
+        } 
         //else hamare paas odd number of elemenes hue honge
         return left_max_heap.top();
     }
