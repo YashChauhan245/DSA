6a9f3081dadc5690a90afd7b7bf06c1c34ac09c6
Yash Chauhan <yashchau.work@gmail.com>
2025-07-28 23:48:36 +0530

    Update postorder.cpp

--- TREES/BINARY TREES/postorder.cpp
+++ TREES/BINARY TREES/postorder.cpp
@@ -1,5 +1,7 @@
 https://leetcode.com/problems/binary-tree-postorder-traversal/
 
+
+APPROACH-1)RECURSIVE
 class Solution {
 public:
     void solver(TreeNode*root,vector<int>&result){
@@ -14,5 +16,47 @@ public:
         vector<int>result;
         solver(root,result);
         return result;
+    }
+};
+
+
+APPROACH-2)ITERATIVE(USING 1 STACK)
+
+class Solution {
+public:
+    void solver(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) return;
+
+        TreeNode* curr = root;
+        TreeNode* temp = nullptr;
+        stack<TreeNode*> st;
+
+        while (curr != NULL || !st.empty()) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                temp = st.top()->right;
+                if (temp == NULL) {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                } else {
+                    curr = temp;
+                }
+            }
+        }
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solver(root, ans);
+        return ans;
     }
 };
