766ebfe56c98ab41de6c509c22e70b641dfe3326
Yash Chauhan <yashchau.work@gmail.com>
2025-07-29 16:03:09 +0530

    Create balance binary tree.cpp

---
+++ TREES/BINARY TREES/balance binary tree.cpp
@@ -0,0 +1,50 @@
+https://leetcode.com/problems/balanced-binary-tree/description/
+
+APPROACH--1) o(N2)
+
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) return 0;
+        return 1 + max(height(root->left), height(root->right));
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) return true;
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        if (abs(leftHeight - rightHeight) <= 1 &&
+            isBalanced(root->left) &&
+            isBalanced(root->right)) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+
+
+APPROACH-2)O(N)
+class Solution {
+public:
+    // Returns height if balanced, else -1
+    int check(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int left = check(root->left);
+        if (left == -1) return -1; // Left not balanced
+
+        int right = check(root->right);
+        if (right == -1) return -1; // Right not balanced
+        
+        if (abs(left - right) > 1) return -1; // This node not balanced
+        return 1 + max(left, right); // Return height
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return check(root) != -1;
+    }
+};
