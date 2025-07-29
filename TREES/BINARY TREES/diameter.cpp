200eb61bed86c35eaca5fc70a569bf16c4ca521a
Yash Chauhan <yashchau.work@gmail.com>
2025-07-29 17:02:33 +0530

    Update diameter.cpp

--- TREES/BINARY TREES/diameter.cpp
+++ TREES/BINARY TREES/diameter.cpp
@@ -1,1 +1,25 @@
 https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+
+class Solution {
+public:
+    int maxDiameter = 0;
+
+    int height(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        // Update diameter at this node
+        maxDiameter = max(maxDiameter, leftHeight + rightHeight);
+
+        // Return height of current node
+        return 1 + max(leftHeight, rightHeight);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return maxDiameter;
+    }
+};
