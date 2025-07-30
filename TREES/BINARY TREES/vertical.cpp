93bd683fb25ca27c5f860ad5087c08eb0e6afcda
Yash Chauhan <yashchau.work@gmail.com>
2025-07-30 20:27:24 +0530

    Update vertical.cpp

--- TREES/BINARY TREES/vertical.cpp
+++ TREES/BINARY TREES/vertical.cpp
@@ -3,41 +3,45 @@ https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/descript
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // Map to store: verLevel (x) -> level (y) -> set of node values
-        map<int, map<int, multiset<int>>> nodes;
-
-        queue<pair<TreeNode*, pair<int, int>>> q;   //nodes,x,level
-        q.push({root, {0, 0}});  // root is at x=0, y=0
+        // map<x, map<y, multiset<node values>>>
+        map<int, map<int, multiset<int>>> mp;
+        // queue for BFS traversal: stores {node, {x, y}}
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        q.push({root, {0, 0}});  // root at x=0, y=0
 
         while (!q.empty()) {
-            pair<TreeNode*, pair<int, int>> p = q.front(); //auto p=q.front();
+            auto front = q.front();
             q.pop();
 
-            TreeNode* node = p.first;
-            int verLevel = p.second.first;
-            int level = p.second.second;
-            // store this in ds
-            nodes[verLevel][level].insert(node->val);
-           // if left ..push in ques 
+            TreeNode* node = front.first;
+            int x = front.second.first;   // vertical level
+            int y = front.second.second;  // depth level
+            //storing in multiset
+            mp[x][y].insert(node->val);
+             // if left ..push in ques 
             if (node->left) {
-                q.push({node->left, {verLevel - 1, level + 1}});
+                q.push({node->left, {x - 1, y + 1}});
             }
-          // if right push in queue
+            // if right.... push in queue
             if (node->right) {
-                q.push({node->right, {verLevel + 1, level + 1}});
+                q.push({node->right, {x + 1, y + 1}});
             }
         }
+
+        // Construct the result
         vector<vector<int>> result;
-      // traversing map for vertical view
-        for (auto& col : nodes) {
+        // traversing map for vertical view
+        for (auto& col : mp) {
             vector<int> temp;
-          // col.second is multiset in map 
+           // col.second is multiset in map 
           // traverisng each col/vertical levle from top to bootm
             for (auto& row : col.second) {
                 temp.insert(temp.end(), row.second.begin(), row.second.end());
             }
+
             result.push_back(temp);
         }
+
         return result;
     }
 };
