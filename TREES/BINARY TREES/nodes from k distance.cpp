4738c67d1a62f729d491c265fbc25f2da2cf7686
Yash Chauhan <yashchau.work@gmail.com>
2025-08-01 15:16:34 +0530

    Update nodes from k distance.cpp

--- TREES/BINARY TREES/nodes from k distance.cpp
+++ TREES/BINARY TREES/nodes from k distance.cpp
@@ -1,5 +1,8 @@
 https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/submissions/1718473777/
 
+
+APPROACH-1)just storing {int} data type in set;
+
 class Solution {
 public:
     unordered_map<TreeNode*, TreeNode*> parent;
@@ -64,6 +67,75 @@ public:
         
         addParent(root);
         
+        collectKDistanceNodes(target, k, result);
+        return result;
+    }
+};
+
+
+Approach-2)using {TREENODE*} IN SET
+
+class Solution {
+public:
+    // Step 1: Create mapping of child -> parent
+    unordered_map<TreeNode*, TreeNode*> parent;
+
+    void addParent(TreeNode* root) {
+        if (!root) return;
+
+        if (root->left) {
+            parent[root->left] = root;
+            addParent(root->left);
+        }
+        if (root->right) {
+            parent[root->right] = root;
+            addParent(root->right);
+        }
+    }
+
+    // Step 2: BFS from target node to collect all nodes at distance k
+    void collectKDistanceNodes(TreeNode* target, int k, vector<int>& result) {
+        queue<TreeNode*> que;
+        que.push(target);
+
+        unordered_set<TreeNode*> visited;
+        visited.insert(target);
+
+        while (!que.empty()) {
+            int n = que.size();
+            if (k == 0) break;
+
+            while (n--) {
+                TreeNode* curr = que.front();
+                que.pop();
+
+                if (curr->left && !visited.count(curr->left)) {
+                    que.push(curr->left);
+                    visited.insert(curr->left);
+                }
+                if (curr->right && !visited.count(curr->right)) {
+                    que.push(curr->right);
+                    visited.insert(curr->right);
+                }
+                if (parent.count(curr) && !visited.count(parent[curr])) {
+                    que.push(parent[curr]);
+                    visited.insert(parent[curr]);
+                }
+            }
+            k--;
+        }
+
+        // All nodes left in the queue are at distance k
+        while (!que.empty()) {
+            result.push_back(que.front()->val);
+            que.pop();
+        }
+    }
+
+    // Main function
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> result;
+        addParent(root);
         collectKDistanceNodes(target, k, result);
         return result;
     }
