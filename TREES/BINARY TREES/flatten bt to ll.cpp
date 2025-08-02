41d5c692c38c10b5b92ea4c4688337e6f16bfced
Yash Chauhan <yashchau.work@gmail.com>
2025-08-02 17:03:19 +0530

    Update flatten bt to ll.cpp

--- TREES/BINARY TREES/flatten bt to ll.cpp
+++ TREES/BINARY TREES/flatten bt to ll.cpp
@@ -1,4 +1,43 @@
 https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
 
+RECURSIVE APPROACH-)S.C o(n)..
 
-   
+class Solution {
+public:
+    TreeNode* prev = nullptr;
+
+    void flatten(TreeNode* root) {
+        if (!root) return;
+
+        flatten(root->right);
+        flatten(root->left);
+
+        root->right = prev;
+        root->left = nullptr;
+        prev = root;
+    }
+};
+
+
+OPTIMAL->s.c-->o(1)  ..using morris
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->left){
+                TreeNode* pred = curr->left;
+                while(pred->right){
+                    pred = pred->right;
+                }
+                pred->right = curr->right;
+                curr->right = curr->left;
+                curr->left = nullptr;
+            }
+            curr = curr->right;
+        }
+    }
+};
+
+
