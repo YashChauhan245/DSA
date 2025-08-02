10ce9a8c14e1d403f4d9d3863fd5926792a68a0c
Yash Chauhan <yashchau.work@gmail.com>
2025-08-02 22:32:40 +0530

    Update search in bst.cpp

--- TREES/BINARY SEARCH TREES/search in bst.cpp
+++ TREES/BINARY SEARCH TREES/search in bst.cpp
@@ -1,3 +1,19 @@
 https://leetcode.com/problems/search-in-a-binary-search-tree/description/
 
-
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==val){
+            return root;
+        }
+        if(val<root->val){
+            return searchBST(root->left,val);
+        }
+        else{
+            return searchBST(root->left,val);
+        }
+    }
+};
