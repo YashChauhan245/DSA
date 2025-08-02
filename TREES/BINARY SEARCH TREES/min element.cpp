5050733f2b0d4f6ad9f8d825239cfc7c6f336688
Yash Chauhan <yashchau.work@gmail.com>
2025-08-02 22:15:33 +0530

    Create min element.cpp

---
+++ TREES/BINARY SEARCH TREES/min element.cpp
@@ -0,0 +1,15 @@
+https://www.geeksforgeeks.org/problems/minimum-element-in-bst/1
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // Code here
+        if(root==NULL){
+            return -1;
+        }
+        while(root->left!=NULL){
+            root=root->left;
+        }
+    return root->data;
+    }
+};
