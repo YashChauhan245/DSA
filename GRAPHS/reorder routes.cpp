df6a0e684367188866a6280bc82a0bc7eca41d09
Yash Chauhan <yashchau.work@gmail.com>
2025-08-10 00:27:34 +0530

    Update reorder routes.cpp

--- GRAPHS/reorder routes.cpp
+++ GRAPHS/reorder routes.cpp
@@ -1,3 +1,58 @@
 https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
 
+//DFS - Using Visited Array
+class Solution {
+public:
+    int count = 0;
+    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj, vector<bool>& visited) {
+      //checking node already visited to shi hai 
+        visited[node] = true;
+        
+        for (auto& [child, sign] : adj[node]) {
+            if (!visited[child]) {
+              //if sign 1 hai(i.e asli node hai) to count ++ ellse just skip 
+                count += sign;
+                dfs(child, node, adj, visited);
+            }
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& connection : connections) {
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], 0});
+        }
+        vector<bool> visited(n, false);
+        dfs(0, -1, adj, visited);
+        return count;
+    }
+};
+
+//DFS - Without using Visited Array because there will be no cycle and hence this is undirected graph
+class Solution {
+public:
+    int count = 0;
+    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj) {
+
+        for (auto& [child, sign] : adj[node]) {
+            if (child != parent) {
+                count += sign;
+                dfs(child, node, adj);
+            }
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& connection : connections) {
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], 0});
+        }
+ 
+        dfs(0, -1, adj);
+        return count;
+    }
+};
+
 
