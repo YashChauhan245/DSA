aab761b6c61e2ec10949e456c1ab45633e5ee882
Yash Chauhan <yashchau.work@gmail.com>
2025-08-12 23:42:19 +0530

    Create eventual safe states.cpp

---
+++ GRAPHS/eventual safe states.cpp
@@ -0,0 +1,47 @@
+https://leetcode.com/problems/find-eventual-safe-states/
+
+
+Approach-1 (Using Same code of DFS Cycle Check in directed Graph)
+T.C : O(V+E)
+S.C : O(V+E)
+
+
+class Solution {
+public:
+    bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
+        
+        for(int &v : adj[u]) {
+            //if not visited, then we check for cycle in DFS
+            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
+                return true;
+            else if(inRecursion[v] == true)
+                return true;
+        }
+        
+        inRecursion[u] = false;
+        return false;
+        
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<bool> visited(V, false);
+        vector<bool> inRecursion(V, false);
+        
+        for(int i = 0; i<V; i++) {
+            if(!visited[i])
+                isCycleDFS(graph, i, visited, inRecursion);
+        }
+        
+        vector<int> safeNodes;
+      //store all false value from recursive state after last iteration 
+        for(int i = 0; i<V; i++) {
+            if(!inRecursion[i])
+                safeNodes.push_back(i);
+        }
+        
+        return safeNodes;
+    }
+};
