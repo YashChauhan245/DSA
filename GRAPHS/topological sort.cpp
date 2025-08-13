2a4138912bf529b143737a59363f8cacd078e2b6
Yash Chauhan <yashchau.work@gmail.com>
2025-08-13 19:45:39 +0530

    Update topological sort.cpp

--- GRAPHS/topological sort.cpp
+++ GRAPHS/topological sort.cpp
@@ -47,4 +47,46 @@ class Solution {
 
 
 ***BFS***
-
+class Solution {
+  public:
+    vector<int> topoSort(int N, vector<vector<int>>& edges) {
+        // Step 1: Convert edge list to adjacency list
+        vector<int> adj[N];
+        for (auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj[u].push_back(v);
+        }
+        
+        // Step 2: Compute indegree of each vertex
+        vector<int> indegree(N, 0);
+        for (int u = 0; u < N; u++) {
+            for (int v : adj[u]) {
+                indegree[v]++;
+            }
+        }
+        
+        // Step 3: Push vertices with indegree 0 into queue
+        queue<int> que;
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0)
+                que.push(i);
+        }
+        
+        // Step 4: BFS to get topological order
+        vector<int> result;
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            result.push_back(u);
+            
+            for (int v : adj[u]) {
+                indegree[v]--;
+                if (indegree[v] == 0)
+                    que.push(v);
+            }
+        }
+        
+        return result;
+    }
+};
