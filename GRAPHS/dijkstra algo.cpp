40c833c89743eff0bc00e7c078f4f126b4b78bab
Yash Chauhan <yashchau.work@gmail.com>
2025-08-15 23:22:01 +0530

    Update dijkstra algo.cpp

--- GRAPHS/dijkstra algo.cpp
+++ GRAPHS/dijkstra algo.cpp
@@ -43,7 +43,50 @@ https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-mat
 
 
 
-***USING SET***
+***USING SET***  --->store unique element adn ascending at the top 
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int,int>>> adj(V);
+        for (auto &e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w}); // remove for directed
+        }
+        
+        vector<int> dist(V, 1e9);
+        dist[src] = 0;
+        
+        set<pair<int,int>> st; // {distance, node}
+        st.insert({0, src});
+        
+        while (!st.empty()) {
+            auto it = *st.begin();
+            int dis = it.first;
+            int node = it.second;
+            st.erase(st.begin());
+            
+            for (auto &nbr : adj[node]) {
+                int adjnode = nbr.first;
+                int edgewt = nbr.second;
+                
+                if (dis + edgewt < dist[adjnode]) {
+                    //***imp point
+                    // Remove old record if exists
+                    if (dist[adjnode] != 1e9) {
+                        st.erase({dist[adjnode], adjnode});
+                    }
+                    dist[adjnode] = dis + edgewt;
+                    st.insert({dist[adjnode], adjnode});
+                }
+            }
+        }
+        
+        return dist;
+    }
+};
+
 
 
 
