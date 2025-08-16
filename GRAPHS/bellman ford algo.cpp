096a261e2067d6917c42bc473f5a3f5c4f11b8d4
Yash Chauhan <yashchau.work@gmail.com>
2025-08-16 18:00:14 +0530

    Create bellman ford algo.cpp

---
+++ GRAPHS/bellman ford algo.cpp
@@ -0,0 +1,33 @@
+https://www.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        vector<int>dist(V,1e8);
+        //now ill iterate for relaxation (V-1) time
+        for(int i=0;i<V;i++){
+            dist[src]=0;
+            //relaxation at all the edges
+            for(auto it:edges){
+                int u=it[0];
+                int v=it[1];
+                int wt=it[2];
+                if(dist[u]!=1e8 &&dist[u]+wt<dist[v]){
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        } 
+        //nth iteration for checking negative cycle
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            if(dist[u]!=1e8 &&dist[u]+wt<dist[v]){
+                //IF NEW SHORTEST DISTANCE FOUND 
+                //RETURN -1
+                return {-1};
+            }
+        }
+        return dist;
+    }
+};
