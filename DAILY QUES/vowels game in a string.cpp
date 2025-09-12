7a8091cd0c463b32731dd6727350203b31b93646
Yash Chauhan <yashchau.work@gmail.com>
2025-09-12 22:33:53 +0530

    Update vowels game in a string.cpp

--- DAILY QUES/vowels game in a string.cpp
+++ DAILY QUES/vowels game in a string.cpp
@@ -1,1 +1,30 @@
 https://leetcode.com/problems/vowels-game-in-a-string/?envType=daily-question&envId=2025-09-12
+
+
+//Approach-1)
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+
+        for(char &ch : s) {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+//Approach-2)(using stl)
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        auto lambda = [](char ch) {
+            return string("aeiou").find(ch) != string::npos;
+        };
+
+        return any_of(s.begin(), s.end(), lambda);
+    }
+};
