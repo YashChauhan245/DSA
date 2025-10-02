4234d252e4a12ead99d5128c6e3dadc204708691
Yash Chauhan <yashchau.work@gmail.com>
2025-10-03 01:51:32 +0530

    Update water bottles ll.cpp

--- DAILY QUES/water bottles ll.cpp
+++ DAILY QUES/water bottles ll.cpp
@@ -1,1 +1,17 @@
 https://leetcode.com/problems/water-bottles-ii/description/?envType=daily-question&envId=2025-10-02
+
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drunk=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            //so exchange possible
+            empty-=numExchange;
+            numExchange++;
+            numBottles++;//drunk++
+            empty++;
+        }
+        return numBottles;//drunk++
+    }
+};
