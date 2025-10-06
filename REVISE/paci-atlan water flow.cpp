15d509dc80cd89795b8eecaffcf18b9b06b0f7cb
Yash Chauhan <yashchau.work@gmail.com>
2025-10-06 14:45:45 +0530

    Update paci-atlan water flow.cpp

--- REVISE/paci-atlan water flow.cpp
+++ REVISE/paci-atlan water flow.cpp
@@ -1,55 +1,66 @@
 https://leetcode.com/problems/pacific-atlantic-water-flow/?envType=daily-question&envId=2025-10-05
-
 class Solution {
 public:
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+    void DFS(vector<vector<int>>& heights, int i, int j, int prevCellVal, vector<vector<bool>>& visited) {
+        if(i < 0 || i >= heights.size() || j < 0 || j >= heights[0].size()) { //invalid cell
+            return;
+        }
+
+        if(heights[i][j] < prevCellVal || visited[i][j])
+            return;
+
+        visited[i][j] = true;
+        for(auto &dir : directions) {
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            DFS(heights, i_, j_, heights[i][j], visited);
+        }
+
+    }
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int rows = heights.size();
-        int cols = heights[0].size();
+        int m = heights.size(); //rows
+        int n = heights[0].size(); //cols
 
-        vector<vector<bool>> pacificVisited(rows, vector<bool>(cols, false));
-        vector<vector<bool>> atlanticVisited(rows, vector<bool>(cols, false));
-
-        // DFS from Pacific (left and top borders)
-        for (int i = 0; i < rows; i++) {
-            dfs(i, 0, heights, pacificVisited, rows, cols);
-            dfs(i, cols - 1, heights, atlanticVisited, rows, cols);
-        }
-
-        for (int j = 0; j < cols; j++) {
-            dfs(0, j, heights, pacificVisited, rows, cols);
-            dfs(rows - 1, j, heights, atlanticVisited, rows, cols);
-        }
-        //common from both the oceans
         vector<vector<int>> result;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (pacificVisited[i][j] && atlanticVisited[i][j]) {
+
+        vector<vector<bool>> pacificVisited(m, vector<bool>(n, false)); //pacificVisited[i][j] = true, means [i][j] water can go to Pacific //m*n
+        vector<vector<bool>> atlanticVisited(m, vector<bool>(n, false)); //atlanticVisited[i][j] = true, means [i][j] water can go to atlantic //m*n
+        //T.C : O(m*n)
+        //S.C : O(m*n)
+
+
+        //Top Row and Bottom Row
+        //Top Row : Pacific connected already
+        //Bottom Row : atlantic connected already
+
+        for(int j = 0; j < n; j++) {
+            DFS(heights, 0, j, INT_MIN, pacificVisited); //Top Row
+            DFS(heights, m-1, j, INT_MIN, atlanticVisited); //Top Row
+        }
+
+        //First col and last column
+        //First col : Pacific connected already
+        //Last col : atlantic connected already
+        for(int i = 0; i < m; i++) {
+            DFS(heights, i, 0, INT_MIN, pacificVisited); //First column
+            DFS(heights, i, n-1, INT_MIN, atlanticVisited); //Last Column
+        }
+
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(pacificVisited[i][j] && atlanticVisited[i][j]) {
                     result.push_back({i, j});
                 }
             }
         }
+
         return result;
     }
-
-private:
-//dfs code
-    void dfs(int x, int y, vector<vector<int>>& heights,
-             vector<vector<bool>>& visited, int rows, int cols) {
-        visited[x][y] = true;
-
-        vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-        
-        for (auto [dx, dy] : directions) {
-            int newX = x + dx;
-            int newY = y + dy;
-
-            if (isValid(newX, newY, rows, cols) && !visited[newX][newY] 
-                && heights[newX][newY] >= heights[x][y]) {
-                dfs(newX, newY, heights, visited, rows, cols);
-            }
-        }
-    }
-    bool isValid(int x, int y, int rows, int cols) {
-        return (x >= 0 && y >= 0 && x < rows && y < cols);
-    }
 };
+
+
