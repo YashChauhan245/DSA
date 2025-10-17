1a7c20046308670fc0ab199407eaf25163d42ff7
Yash Chauhan <yashchau.work@gmail.com>
2025-10-18 02:29:11 +0530

    Update smallest missing int. after operations.cpp

--- DAILY QUES/smallest missing int. after operations.cpp
+++ DAILY QUES/smallest missing int. after operations.cpp
@@ -1,26 +1,22 @@
 https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations/description/
 
+
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        unordered_map<int, int> mp;
+        unordered_map<int, int> mp;//with freq
 
         for(int &num : nums) {
-            int r = ((num % value) + value) % value;
-
+            int r = ((num % value) + value) % value;//+val)%val--->for negative numbers
+            //e.g->(-12%5)-->-2(+5)--->3%5-->3 OR -12+5-->-7+5--->-2+5==3
             mp[r]++;
         }
-
-        int MEX = 0;
-
+        int MEX = 0;//as non neg starts from 0--n
         while(mp[(MEX % value)] > 0) {
-            mp[(MEX % value)]--;
+            mp[(MEX % value)]--;//freq--
 
-            MEX++;
+            MEX++;//index++
         }
-
         return MEX;
     }
 };
-
-
