f6b36adce26b4cf261dbc59497a151b76e6a0b88
Yash Chauhan <yashchau.work@gmail.com>
2025-10-18 02:16:33 +0530

    Create smallest missing int. after operations.cpp

---
+++ DAILY QUES/smallest missing int. after operations.cpp
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations/description/
+
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        unordered_map<int, int> mp;
+
+        for(int &num : nums) {
+            int r = ((num % value) + value) % value;
+
+            mp[r]++;
+        }
+
+        int MEX = 0;
+
+        while(mp[(MEX % value)] > 0) {
+            mp[(MEX % value)]--;
+
+            MEX++;
+        }
+
+        return MEX;
+    }
+};
+
+
