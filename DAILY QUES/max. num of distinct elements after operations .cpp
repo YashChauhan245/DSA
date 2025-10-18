1127be6ef88f3189f26c5111f03edf507ace2ee0
Yash Chauhan <yashchau.work@gmail.com>
2025-10-18 18:51:38 +0530

    Create max. num of distinct elements after operations .cpp

---
+++ DAILY QUES/max. num of distinct elements after operations .cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/submissions/1805006219/
+
+//sort-->nums[i-k] -->if already present then prev+1 
+
+class Solution {
+public:
+    int maxDistinctElements(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        sort(begin(nums), end(nums));
+
+        int count = 0;
+
+        int prev = INT_MIN;
+
+        for(int i = 0; i < n; i++) {
+            int minVal = nums[i] - k;
+
+            if(prev < minVal) {
+                //nums[i] = nums[i]-k;
+                prev = minVal;
+                count++;
+            } else if(prev < nums[i] + k) {
+                //nums[i] = prev+1;
+                prev = prev+1;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
