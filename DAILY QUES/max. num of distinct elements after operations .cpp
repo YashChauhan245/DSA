63e60e50e6ddb9fcf8c105a89486dc544e511c82
Yash Chauhan <yashchau.work@gmail.com>
2025-10-18 19:06:55 +0530

    Update max. num of distinct elements after operations .cpp

--- DAILY QUES/max. num of distinct elements after operations .cpp
+++ DAILY QUES/max. num of distinct elements after operations .cpp
@@ -1,8 +1,9 @@
 https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/submissions/1805006219/
 
 //sort-->nums[i-k] -->if already present then prev+1 but but prev+1 >limit(nums[i]-k,nums[i]+1) -->return k
-//e.g -->[4,4,4,4] k=1-->so limit is [4-1;4+1]=[3,5] So, output=[3,4,5,4]
+//e.g -->[4,4,4,4] k=1-->so limit is [4-1;4+1]=[3,5] So, final arr=[3,4,5,4]--->output-->3
 
+//e.g=[1,1,1,2,2,2,4,4,4,4]=[-1,0,1,2,3,4,5,4,4]===>output-->8
 
 
 class Solution {
