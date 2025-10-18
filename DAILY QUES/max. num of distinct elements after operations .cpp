fe8ea300494be421003bdf44892bce8696acc3c5
Yash Chauhan <yashchau.work@gmail.com>
2025-10-18 19:01:35 +0530

    Update max. num of distinct elements after operations .cpp

--- DAILY QUES/max. num of distinct elements after operations .cpp
+++ DAILY QUES/max. num of distinct elements after operations .cpp
@@ -1,6 +1,9 @@
 https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/submissions/1805006219/
 
-//sort-->nums[i-k] -->if already present then prev+1 
+//sort-->nums[i-k] -->if already present then prev+1 but but prev+1 >limit(nums[i]-k,nums[i]+1) -->return k
+//e.g -->[4,4,4,4] k=1-->so limit is [4-1;4+1]=[3,5] So, output=[3,4,5,4]
+
+
 
 class Solution {
 public:
