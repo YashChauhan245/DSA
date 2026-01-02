f51b0ac46ef04d4db9839754d74036761430a228
PhizeR <yashchau.work@gmail.com>
2026-01-02 18:15:20 +0530

    Update find first and last position of element.cpp

--- BINARY SEARCH/find first and last position of element.cpp
+++ BINARY SEARCH/find first and last position of element.cpp
@@ -3,43 +3,46 @@ https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-
 
 class Solution {
 public:
+
     int findFirst(vector<int>& nums, int target) {
-        int ans = -1, s = 0, e = nums.size() - 1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                e = mid - 1; // look left
+        int s=0,e=nums.size()-1, ans=-1;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                e=mid-1;
             }
-            else if (nums[mid] < target) {
-                s = mid + 1;
+            else if(nums[mid]<target){
+                s=mid+1;
             }
-            else {
-                e = mid - 1;
+            else{
+                e=mid-1;
             }
-        }
+        } 
         return ans;
     }
 
     int findLast(vector<int>& nums, int target) {
-        int ans = -1, s = 0, e = nums.size() - 1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                s = mid + 1; // look right
+        int s=0,e=nums.size()-1, ans=-1;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                s=mid+1;
             }
-            else if (nums[mid] < target) {
-                s = mid + 1;
+            else if(nums[mid]<target){
+                s=mid+1;
             }
-            else {
-                e = mid - 1;
+            else{
+                e=mid-1;
             }
-        }
+        } 
         return ans;
     }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {findFirst(nums, target), findLast(nums, target)};
+        return {findFirst(nums,target),findLast(nums,target)};
     }
+
 };
