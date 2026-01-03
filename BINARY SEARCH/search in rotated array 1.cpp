e9079537f1d42e24cb2b5923bdd20a83c5465b8f
PhizeR <yashchau.work@gmail.com>
2026-01-03 23:01:18 +0530

    Update search in rotated array 1.cpp

--- BINARY SEARCH/search in rotated array 1.cpp
+++ BINARY SEARCH/search in rotated array 1.cpp
@@ -3,29 +3,30 @@ https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-    int s = 0, e = nums.size() - 1;
+        int s=0,e=nums.size()-1;
 
-    while (s <= e) {
-        int mid = (s + e) / 2;
-
-        if (nums[mid] == target) return mid;
-
-        // Left half is sorted
-        if (nums[s] <= nums[mid]) {
-            if (nums[s] <= target && target < nums[mid])
-                e = mid - 1;
-            else
-                s = mid + 1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && nums[mid]>target){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(target<=nums[e] && target>nums[mid]){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
         }
-        // Right half is sorted
-        else {
-            if (nums[mid] < target && target <= nums[e])
-                s = mid + 1;
-            else
-                e = mid - 1;
-        }
-    }
-    return -1;
-        
+        return -1;
     }
 };
