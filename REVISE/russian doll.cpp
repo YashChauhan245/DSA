5b147ac8d2e7e2f17fb74c56e069ed4262cef5cd
PhizeR <yashchau.work@gmail.com>
2026-01-06 18:40:07 +0530

    Update russian doll.cpp

--- REVISE/russian doll.cpp
+++ REVISE/russian doll.cpp
@@ -5,33 +5,43 @@ https://leetcode.com/problems/russian-doll-envelopes/description/?envType=study-
 
 class Solution {
 public:
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        // Sort: smaller width first, if width same then bigger height first
-        sort(envelopes.begin(), envelopes.end(), [](auto &a, auto &b){
-            if(a[0] == b[0]) {
-                return a[1] > b[1];
-            }
-            return a[0] < b[0];
+    int maxEnvelopes(vector<vector<int>>& a) {
+        int n = a.size();
+
+        // If there are no envelopes, we can't nest anything, so answer is 0
+        if(n == 0) return 0;
+
+        // Sort envelopes:
+        // - First by width in increasing order (small → big)
+        // - If two envelopes have same width, sort height in decreasing order (big → small)
+        sort(a.begin(), a.end(), [](vector<int>& x, vector<int>& y){
+            if(x[0] == y[0]) 
+                return x[1] > y[1]; // Taller height comes first when width is same
+            return x[0] < y[0]; // Smaller width comes first
         });
 
-        // LIS using binary search on heights
-        vector<int> lis;
-        for(auto &e : envelopes){
-            int height = e[1];
 
-            // Find correct position to put this height
-            int index = lower_bound(lis.begin(), lis.end(), height) - lis.begin();
+        // (LIS = longest valid nesting based on height after sorting)
+        vector<int> ans;
+        // Push the height of first envelope (after sorting)
+        ans.push_back(a[0][1]);
 
-            // If height is bigger than all in lis, add it
-            if(index == lis.size()) {
-                lis.push_back(height);
-            }
-            // Else replace the existing value to keep lis smallest possible
+        // Start checking from 2nd envelope to last
+        for(int i = 1; i < n; i++) {
+            int h = a[i][1]; // current envelope height
+
+            // If current height is greater than last stored height in ans,
+            // it means we can nest it, so add it to sequence
+            if(h > ans.back()) {
+                ans.push_back(h);
+            } 
+            // Else, find the position where this height should be placed
+            // using binary search (`lower_bound`) to maintain smallest sequence
             else {
-                lis[index] = height;
+                int idx = lower_bound(ans.begin(), ans.end(), h) - ans.begin();
+                ans[idx] = h; // replace the value at found position
             }
         }
-
-        return lis.size();
+        return ans.size();
     }
 };
