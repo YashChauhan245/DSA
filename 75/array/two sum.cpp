aeb2ab57839cf054dd2cdb1579903492e7798cc8
PhizeR <yashchau.work@gmail.com>
2026-03-03 14:38:11 +0530

    Update two sum.cpp

--- 75/array/two sum.cpp
+++ 75/array/two sum.cpp
@@ -17,8 +17,7 @@ public:
 };
 
 
-
-optimized->o(n) & o(n)
+better->o(n) & o(n)
 
 class Solution {
 public:
@@ -35,3 +34,39 @@ public:
         return {};
     }
 };
+
+
+optimised-> o(nlogn) and o(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        // Store value along with original index
+        vector<pair<int, int>> arr;
+        arr.reserve(n);
+        for (int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
+        }
+        // Sort based on values
+        sort(arr.begin(), arr.end());
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left].first + arr[right].first;
+
+            if (sum == target) {
+                return {arr[left].second, arr[right].second};
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return {};
+    }
+};
+
