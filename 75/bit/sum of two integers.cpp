af11b140b6b682472cc3139c4eaf1e244e92c3af
PhizeR <yashchau.work@gmail.com>
2026-03-06 15:24:12 +0530

    Create sum of two integers.cpp

---
+++ 75/bit/sum of two integers.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/sum-of-two-integers/description/
+
+tc,sc-->both o(1)
+
+// steps:a)sum(xor) b) carry(&) c) new carry (carry<<1) d} repeat all steps till carry become 0
+class Solution {
+public:
+    int getSum(int a, int b) {
+        uint32_t carry=a&b;   //so carry must sotre positive value only
+        int sumwithoutcarry=a^b;
+        int newcarry=carry<<1;
+
+        while(carry!=0){
+            carry=sumwithoutcarry & newcarry;
+            sumwithoutcarry=sumwithoutcarry^newcarry;
+            newcarry=carry<<1;
+        }
+       return sumwithoutcarry; 
+    }
+};
