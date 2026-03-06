152ec4abf9f614b42a7f245164cbe5a7ea3c2039
PhizeR <yashchau.work@gmail.com>
2026-03-06 15:52:28 +0530

    Update number of 1 bits.cpp

--- 75/bit/number of 1 bits.cpp
+++ 75/bit/number of 1 bits.cpp
@@ -1,2 +1,34 @@
 https://leetcode.com/problems/number-of-1-bits/description/
 
+//how to find binary of any number---->find and store remainder(%) in binary string and replace quotient(/) wuth new n 
+
+//tc-->o(logn)....divide by 2
+//sc-->o(1)
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ones=0;
+        while(n>0){
+            if(n%2==1){  //remainder
+                ones++;
+            }
+            n=n/2;  //quotient
+        }
+        return ones;
+    }
+};
+
+
+optimised->o(k)
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ones = 0;
+        while(n){
+            n = n & (n - 1);
+            ones++;
+        }
+        return ones;
+    }
+};
