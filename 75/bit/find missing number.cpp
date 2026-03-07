ab0356063aa3d82836af52fef194ba452aae38d2
PhizeR <yashchau.work@gmail.com>
2026-03-07 20:02:20 +0530

    Refactor missing number function with new algorithms
    Refactored missing number algorithm to use XOR method and added binary search approach.

--- 75/bit/find missing number.cpp
+++ 75/bit/find missing number.cpp
@@ -1,17 +1,14 @@
 https://www.geeksforgeeks.org/dsa/find-the-missing-number/
 
-
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int expected = n * (n + 1) / 2;
-
-        int sum = 0;
-        for(int num : nums)
-            sum += num;
-
-        return expected - sum;
+        int sum = nums.size();
+        for(int i = 0; i<nums.size(); i++) {
+            sum ^= i;
+            sum ^= nums[i];
+        }
+        return sum;
     }
 };
 
@@ -29,4 +26,32 @@ public:
 
         return ans;
     }
-};]
+};
+
+
+//Approach-3 (Binary Search)
+//T.C : O(nlogn)
+//S.C : O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int l   = 0;
+        int r   = n-1;
+        int result = n;
+        
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(nums[mid] > mid) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        
+        return result;
+        
+    }
+};
