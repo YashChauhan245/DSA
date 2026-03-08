51f4e0a506c9adcdf977108181849d50646cb6a3
PhizeR <yashchau.work@gmail.com>
2026-03-08 17:19:22 +0530

    Update find missing number.cpp

--- 75/bit/find missing number.cpp
+++ 75/bit/find missing number.cpp
@@ -14,19 +14,8 @@ public:
 
 
 
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans = n;
-
-        for(int i = 0; i < n; i++){
-            ans ^= i ^ nums[i];
-        }
-
-        return ans;
-    }
-};
+// Approach-2 (math)
+    sum of indexes of arr till - total sum of array
 
 
 //Approach-3 (Binary Search)
