b4619d909f72489321cffad85684f8235112aa73
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-11 19:48:17 +0530

    Update longest common subsequence.cpp

--- 75/dp/longest common subsequence.cpp
+++ 75/dp/longest common subsequence.cpp
@@ -1,2 +1,92 @@
 https://leetcode.com/problems/longest-common-subsequence/description/
 
+//recursion 
+
+// class Solution {
+// public:
+
+//     int solver(string text1, string text2,int n,int m,int i,int j){
+//         if(i>=n || j>=m){
+//             return 0;
+//         }
+
+//         //matched
+//         if(text1[i]==text2[j]){
+//             return 1+solver(text1,text2,n,m,i+1,j+1);
+//         }
+
+//         //not matched
+//         //case1 ->skip i
+//         int skip_i=solver(text1,text2,n,m,i+1,j);
+//         //case2->skip j
+//         int skip_j=solver(text1,text2,n,m,i,j+1);
+//         // return max from both
+//         return max(skip_i,skip_j);
+//     }
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n=text1.size();
+//         int m=text2.size();
+
+//         return solver(text1,text2,n,m,0,0);
+//     }
+// };
+
+//top down 
+// class Solution {
+// public:
+
+//     int solver(string &text1, string &text2, int i, int j, vector<vector<int>>& dp){
+        
+//         if(i >= text1.size() || j >= text2.size())
+//             return 0;
+
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+
+//         if(text1[i] == text2[j]){
+//             return dp[i][j] = 1 + solver(text1,text2,i+1,j+1,dp);
+//         }
+
+//         int skip_i = solver(text1,text2,i+1,j,dp);
+//         int skip_j = solver(text1,text2,i,j+1,dp);
+
+//         return dp[i][j] = max(skip_i,skip_j);
+//     }
+
+//     int longestCommonSubsequence(string text1, string text2) {
+
+//         int n = text1.size();
+//         int m = text2.size();
+//         vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+//         return solver(text1,text2,0,0,dp);
+//     }
+// };
+
+
+//bottom up 
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+
+                if(text1[i] == text2[j]){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+
+            }
+        }
+
+        return dp[0][0];
+    }
+};
