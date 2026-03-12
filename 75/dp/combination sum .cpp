899de462284d81bf7132459922b9cf6087e86eec
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-12 22:32:43 +0530

    Update combination sum .cpp

--- 75/dp/combination sum .cpp
+++ 75/dp/combination sum .cpp
@@ -1,2 +1,30 @@
 https://leetcode.com/problems/combination-sum/description/
 
+class Solution {
+public:
+
+   vector<vector<int>> result;
+   void solver(vector<int>& candidates, int target,vector<int>temp,int i){
+        //result return case
+        if(target==0){
+            result.push_back(temp);
+            return;
+        }
+
+        //edge case
+        if(target<0 || i>=candidates.size()){
+            return;
+        }
+        //push ->further check -->popback-->again check with i+1
+        temp.push_back(candidates[i]);
+        solver(candidates,target-candidates[i],temp,i);
+        temp.pop_back();
+        solver(candidates,target,temp,i+1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        solver(candidates,target,temp,0);
+        return result;
+    }
+};
