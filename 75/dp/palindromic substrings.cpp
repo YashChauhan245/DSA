712dd458df7f7b7d79d3f65242ec9daf3c3bf13d
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-15 22:34:59 +0530

    Update palindromic substrings.cpp

--- 75/dp/palindromic substrings.cpp
+++ 75/dp/palindromic substrings.cpp
@@ -3,31 +3,29 @@ https://leetcode.com/problems/palindromic-substrings/
 class Solution {
 public:
 
-    bool isPalindrome(string &s, int l, int r){
-        if(l >= r) return true;
-
-        if(s[l] != s[r]) return false;
-
-        return isPalindrome(s, l+1, r-1);
-    }
-
-    int solve(string &s, int i){
-        int n = s.size();
-
-        if(i == n) return 0;
-
-        int count = 0;
-
-        for(int j = i; j < n; j++){
-            if(isPalindrome(s, i, j)){
-                count++;
-            }
+    bool check(string &s, int i, int j) {
+        if(i >= j) {
+            return true;
         }
-
-        return count + solve(s, i + 1);
+        if(s[i] == s[j]) {
+            return check(s, i+1, j-1);
+        }
+        return false;
     }
 
     int countSubstrings(string s) {
-        return solve(s, 0);
+        int n = s.length();
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) { //check all possible substrings
+                if(check(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
+;
+
