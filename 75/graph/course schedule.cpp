a80978d58a603484983d9dea42def240c2ef37d3
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-16 20:14:58 +0530

    Update course schedule.cpp

--- 75/graph/course schedule.cpp
+++ 75/graph/course schedule.cpp
@@ -1,2 +1,60 @@
 https://leetcode.com/problems/course-schedule/description/
 
+class Solution {
+public:
+    
+    bool topologicalSortCheck(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
+        queue<int> que;
+        
+        int count = 0;
+        //storing element that have by default 0 indegree
+        for(int i = 0; i<n; i++) {
+            if(indegree[i] == 0) {
+                count++;
+                que.push(i);
+            }
+        }
+        
+        
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            
+            for(int &v : adj[u]) {
+                
+                indegree[v]--;
+                
+                if(indegree[v] == 0) {
+                    count++;
+                    que.push(v);
+                }              
+            }          
+        }
+        
+        if(count == n) //I was able to visit all nodes (course)
+            return true; //i.e. I was able to finish all courses
+        
+        return false; //means there was a cycle and I couldn't complete all courses
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        unordered_map<int, vector<int>> adj; //for storing given things       
+        vector<int> indegree(numCourses, 0); //kahn's algo
+        
+        for(auto &vec : prerequisites) { //storing given values
+            int a = vec[0];
+            int b = vec[1];
+            
+            //b ---> a  (topo sort logic)
+            adj[b].push_back(a);
+            
+            //arrow ja raha hai 'a' me  so indegree++
+            indegree[a]++;
+        }
+             
+        //if cycle is present, not possible
+        
+        return topologicalSortCheck(adj, numCourses, indegree);
+    }
+};
