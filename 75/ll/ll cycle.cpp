7336aa4b70e95dc0f7631802424e317517fc8fd0
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-23 17:08:28 +0530

    Create ll cycle.cpp

---
+++ 75/ll/ll cycle.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/linked-list-cycle/description/
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) return false;
+
+        ListNode* slow = head;       
+        ListNode* fast = head;   //not head bcz ist condn me hi true de dega 
+
+        while (fast != nullptr  && fast->next != nullptr) {
+            slow = slow->next;        
+            fast = fast->next->next;
+            if (slow == fast){
+                return true; 
+            }
+        }
+        return false;
+    }
+};
