3810ee157fd94aa77b7fe7f8cd5449283b557858
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-29 18:48:39 +0530

    Create palindromic substrings.cpp

---
+++ 75/string/palindromic substrings.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/palindromic-substrings/description/
+
+
+class Solution {
+public:
+    int count = 0;
+    void check(string& s, int i, int j, int n) {
+        
+        while(i >= 0 && j < n && s[i] == s[j]) {
+            count++;
+            i--; //expanding from center
+            j++; //expanding from center
+        }
+    }
+    int countSubstrings(string s) {
+        int n = s.length();
+        count = 0;
+        
+        for(int i = 0; i<n; i++) {
+            check(s, i, i, n);
+            check(s, i, i+1, n);
+        }
+        return count;
+    }
+};
