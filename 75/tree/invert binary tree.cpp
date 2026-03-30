a3a417fb52c51ab2be55e3838cd400b48ef2bf0a
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-30 17:12:37 +0530

    Implement invert binary tree function

---
+++ 75/tree/invert binary tree.cpp
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/invert-binary-tree/description/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) return root;
+
+        swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
