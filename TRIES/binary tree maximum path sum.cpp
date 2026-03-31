51425e094ab475ed914c4cba15097307a8e9283d
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-03-31 18:27:47 +0530

    Update binary tree maximum path sum.cpp

--- TRIES/binary tree maximum path sum.cpp
+++ TRIES/binary tree maximum path sum.cpp
@@ -13,13 +13,15 @@ public:
         // now cases
 
         // niche hi max mil gyaa but main root ko include nhi kr payege bcz of path ...3d path bn jayega warna 
+        //so i cant return it as answer
         int noreturn=left+right+root->val;
-        //1 if left ya right me se koi ek ho
+        
+        // if left ya right me se koi ek ho
         int onlyone=max(left,right)+root->val;
         // root khud hi max ho
         int rootmax=root->val;
         maxsum=max({maxsum,noreturn,onlyone,rootmax});
-        // imp as we can only return these two ;
+        // imp as we can only return these two and cant return the 1st case ;
         return max(onlyone,rootmax);
     }
     int maxPathSum(TreeNode* root) {
