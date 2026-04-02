6ce85efce8515e0847dd9b450f71aa57e2e4c8d6
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-04-02 17:25:56 +0530

    Create lca.cpp

---
+++ 75/tree/lca.cpp
@@ -0,0 +1,34 @@
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        // If root is p or q, return root
+        if(root == p || root == q) {
+            return root;
+        }
+
+        // If p and q are on opposite sides
+        if((p->val > root->val && q->val < root->val) || 
+           (p->val < root->val && q->val > root->val)) {
+            return root;
+        }
+
+        // Both nodes on right
+        if(p->val > root->val && q->val > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+
+        // Both nodes on left
+        if(p->val < root->val && q->val < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        //if all if conditions fail ->return null or root ;
+        return NULL;   //return root 
+    }
+};
