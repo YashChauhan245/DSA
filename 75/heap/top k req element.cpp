dbd2dde2a7b4c5174b9343197f3b0f8fca0ccf9d
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-04-03 22:54:11 +0530

    Create top k req element.cpp

---
+++ 75/heap/top k req element.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minhp;//lowest freq element at top
+        vector<int>ans;
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        } 
+      
+        for (auto it : freq) {
+            minhp.push({it.second, it.first});
+            if(minhp.size()>k){
+                minhp.pop();
+            }
+        }
+        while(!minhp.empty()){
+            ans.push_back(minhp.top().second);
+            minhp.pop();
+        }
+        return ans;
+    }
+};
