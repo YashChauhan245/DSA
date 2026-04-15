4213f5fb019ccb2414ced91098d7e7447b4f0cb4
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-04-15 18:35:58 +0530

    Create shortest distance to target string in a circular array.cpp

---
+++ DAILY QUES/shortest distance to target string in a circular array.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/?envType=daily-question&envId=2026-04-15
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        int ans = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            if(words[i] == target) {
+                int diff = abs(i - startIndex);
+                int dist = min(diff, n - diff);
+                ans = min(ans, dist);
+            }
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
