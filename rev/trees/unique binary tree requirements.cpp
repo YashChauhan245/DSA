4e6a19b75de1fa0da8f2e2681a8cfaafa175b0e6
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-04-26 22:38:56 +0530

    Update unique binary tree requirements.cpp

--- rev/trees/unique binary tree requirements.cpp
+++ rev/trees/unique binary tree requirements.cpp
@@ -1,1 +1,15 @@
 https://www.geeksforgeeks.org/problems/unique-binary-tree-requirements/1
+
+
+//Intution-->No inorder → No unique tree
+class Solution {
+  public:
+    bool isPossible(int a, int b) {
+        if ((a == 1 && b == 2) || 
+            (a == 2 && b == 1) || 
+            (a == 2 && b == 3) || 
+            (a == 3 && b == 2))
+            return true;
+        return false;
+    }
+};
