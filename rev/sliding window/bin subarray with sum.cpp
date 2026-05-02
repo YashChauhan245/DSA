143e5dca7de65e19eb08b279227d87a89ea00bd6
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-02 22:56:00 +0530

    Update bin subarray with sum.cpp

--- rev/sliding window/bin subarray with sum.cpp
+++ rev/sliding window/bin subarray with sum.cpp
@@ -1,6 +1,43 @@
 https://leetcode.com/problems/binary-subarrays-with-sum/description/
 
 
+//Approach-1 (Simple prefix sum) - These kind of Qns can always be solved using Map and cumulative sum technique
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        
+        unordered_map<int, int> mp;
+        
+        int count   = 0;
+        int currSum = 0;
+        mp[0] = 1;
+        for(int &num : nums) {
+            
+            currSum += num;
+
+            if(mp.find(currSum - goal) != mp.end()) {
+                count += mp[currSum - goal];
+            }
+            
+            mp[currSum]++;
+            
+            
+        }
+        
+        return count;
+    }
+};
+
+
+//Approach-2 (Using Sliding Window)
+//This could be a very easy sliding window but the presence of 0 makes it a little tricky.
+//Example : [0, 0, 0, 1, 1] -> Total subarrays having goal = 2 will be 4 - {0, 0, 0, 1, 1},  {0, 0, 1, 1}, {0, 1, 1}, {1, 1}
+//So, simply find the count of zeros before the sum has reached goal i.e. 3 (Now, 1+1 = goal, so total subarrays = prefix_zeros + 1 = 3 + 1 = 4)
+//T.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
@@ -33,3 +70,4 @@ public:
         return count;
     }
 };
+
