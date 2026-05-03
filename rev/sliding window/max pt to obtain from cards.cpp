29c3d8947f99e31630038d0b8c923c3a2aa62f2f
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-03 19:48:35 +0530

    Update max pt to obtain from cards.cpp

--- rev/sliding window/max pt to obtain from cards.cpp
+++ rev/sliding window/max pt to obtain from cards.cpp
@@ -1,1 +1,32 @@
 https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/description/
+
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+
+        // Calculate initial sum by picking first k cards from front
+        int total = 0;
+        for (int i = 0; i < k; ++i) {
+            total += cardPoints[i];
+        }
+
+        // Store current max score
+        int maxPoints = total;
+
+        // Move the window from front to back k times
+        for (int i = 0; i < k; ++i) {
+            // Subtract card from front
+            total -= cardPoints[k - 1 - i];
+
+            // Add card from back
+            total += cardPoints[n - 1 - i];
+
+            // Update max score 
+            maxPoints = max(maxPoints, total);
+        }
+
+        return maxPoints;
+    }
+};
