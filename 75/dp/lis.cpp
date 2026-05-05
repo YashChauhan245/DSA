4095b0185aff26205f47d37aab4af072296e3608
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-05 22:12:16 +0530

    Update lis.cpp

--- 75/dp/lis.cpp
+++ 75/dp/lis.cpp
@@ -1,8 +1,38 @@
 https://leetcode.com/problems/longest-increasing-subsequence/
 
+//LIS-->WITH BINARY SEARCH 
+//T.C : O(nlogn)
+//S.C : O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sorted;
+        
+        for(int i = 0; i<n; i++) {
+            /*
+			     Why lower bound ?
+				 We want increasing subsequence and  hence 
+				 we want to eliminate the duplicates as well.
+				 lower_bound returns iterator to "next greater or equal to"
+			*/
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]);
+            
+            if(it == end(sorted))
+                sorted.push_back(nums[i]); //greatest : so insert it
+            else
+                *it = nums[i]; //replace
+        }
+        
+        return (int)sorted.size();
+    }
+};
+
+
+
+//LIS-->WITH DP
 
 //recursive 
-
 // class Solution {
 // public:
 //     int solver(vector<int>& nums,int n,int i,int p){
