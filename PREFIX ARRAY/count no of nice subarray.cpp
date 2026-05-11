593d4d526c3a80c7fab7a772770199ef7de6050e
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-11 20:29:39 +0530

    Update count no of nice subarray.cpp

--- PREFIX ARRAY/count no of nice subarray.cpp
+++ PREFIX ARRAY/count no of nice subarray.cpp
@@ -1,5 +1,6 @@
 https://leetcode.com/problems/count-number-of-nice-subarrays/description/
 
+//prefix sum
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
@@ -19,5 +20,43 @@ public:
             mp[currSum]++;
         }
         return count;
+    }
+};
+
+
+//sliding window 
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int oddCount = 0;
+        int count    = 0;
+        int result   = 0;
+
+        int i = 0;
+        int j = 0;
+        //Best example to understand the use of count variable - {2, 1, 2, 1}, output : 6
+        while(j < n) {
+
+            if(nums[j] % 2 != 0) { //ODD
+                oddCount++;
+                count    = 0; //We need to reset this to avoid over counting. Example : [1,1,2,1,1], k = 3
+            }
+
+            while(oddCount == k) {
+                count++;
+
+                if(i < n && nums[i] % 2 == 1) { //ODD
+                    oddCount--;
+                }
+                i++;
+            }
+
+            result += count;
+            j++;
+        }
+
+        return result;
     }
 };
