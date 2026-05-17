c113a9ac4ff3915838aa1af34e72751adb714261
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-17 12:50:59 +0530

    Create 3.largest local values in a matrix.cpp

---
+++ CONTEST/502/3.largest local values in a matrix.cpp
@@ -0,0 +1,51 @@
+https://leetcode.com/problems/largest-local-values-in-a-matrix-ii/
+
+//MY APPROACH-->TLE
+class Solution {
+public:
+    int countLocalMaximums(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // store coordinates of each value
+        vector<pair<int,int>> pos[201];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                pos[matrix[i][j]].push_back({i, j});
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int x = matrix[i][j];
+                // skip zero cells
+                if (x == 0)
+                    continue;
+                bool greaterFound = false;
+                // check only greater values
+                for (int val = x + 1; val <= 200 && !greaterFound; val++) {
+                    for (auto &[r, c] : pos[val]) {
+                        int dr = abs(r - i);
+                        int dc = abs(c - j);
+                        // inside allowed range
+                        if (dr <= x && dc <= x) {
+                            // ignore corners
+                            if (dr == x && dc == x)
+                                continue;
+                            greaterFound = true;
+                            break;
+                        }
+                    }
+                }
+                // no greater value found
+                if (!greaterFound)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
