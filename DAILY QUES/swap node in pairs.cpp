be853ac57dac12dbcdf0397e0883eddad7cceef9
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-22 22:00:37 +0530

    Create swap node in pairs.cpp

---
+++ DAILY QUES/swap node in pairs.cpp
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* nextNode = head->next;
+        head->next = swapPairs(head->next->next);
+        nextNode->next = head;
+        return nextNode;
+    }
+};
