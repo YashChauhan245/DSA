ae49d33c80f1b3c3af950554ef2c6309847656dd
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-05-29 22:27:57 +0530

    Create 2.password strength.cpp

---
+++ CONTEST/503/2.password strength.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/password-strength/description/
+
+class Solution {
+public:
+    int passwordStrength(string password) {
+        unordered_set<char> st(password.begin(), password.end());//no duplicates
+        int ans = 0;
+
+        for (char ch : st) {
+            if(islower(ch)){
+                ans += 1;
+            }
+            else if(isupper(ch)){
+                ans += 2;
+            }
+            else if(isdigit(ch)){
+                ans += 3;
+            }
+            else{
+                ans += 5;
+            }
+        }
+        return ans;
+    }
+};
