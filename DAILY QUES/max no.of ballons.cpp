a4c28f36e26faaad975445a170802d53c48420be
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-06-22 14:40:06 +0530

    Create max no.of ballons.cpp

---
+++ DAILY QUES/max no.of ballons.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/maximum-number-of-balloons/description/?envType=daily-question&envId=2026-06-22
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> freq(26, 0);
+
+        for (char c : text)
+            freq[c - 'a']++;
+
+        return min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2, //as balloon haas 2 L & O
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a']
+        });
+    }
+};
