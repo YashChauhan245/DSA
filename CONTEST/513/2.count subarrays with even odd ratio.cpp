a2a5dd9fc586c5525a8971e0dd7eb78cca7ad069
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-08-02 18:22:37 +0530

    Create 2.count subarrays with even odd ratio.cpp

---
+++ CONTEST/513/2.count subarrays with even odd ratio.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/count-subarrays-with-even-odd-ratio-i/
+
+class Solution {
+public:
+    int countRatioSubarrays(vector<int>& nums, int a, int b) {
+        int n=nums.size();
+        int ans=0;
+        
+        for (int i = 0; i < n; i++) {
+            int even = 0, odd = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0)
+                    even++;
+                else
+                    odd++;
+
+                if (odd > 0 && even * b <= odd * a)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
