93692099375f029fc8f5b01b1ff7ce39444a45d9
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-09-02 15:28:00 +0530

    Create construct uniform parity array l.cpp

---
+++ DAILY QUES/construct uniform parity array l.cpp
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/construct-uniform-parity-array-i/description/?envType=daily-question&envId=2026-09-02
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
