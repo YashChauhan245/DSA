fd987daa1f4db25109537fbe9f90102f46417cf3
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-09-06 17:56:24 +0530

    Create 1.count rotations with exactly k equal adjacent pairs.cpp

---
+++ CONTEST/518/1.count rotations with exactly k equal adjacent pairs.cpp
@@ -0,0 +1,25 @@
+https://leetcode.com/problems/count-rotations-with-exactly-k-equal-adjacent-pairs/description/
+
+
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n=s.size();
+        int count=0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]==s[(i+1)%n]){
+                count++;
+            }
+        }
+        if(k==count){
+            return n-count;
+        }
+        if(k==count-1){
+            return count;
+        }
+
+        return 0;
+            
+    }
+};
