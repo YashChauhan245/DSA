8108f3fd20d0fa231cd623b23a245d891177c479
Naas Holiyaaaa <yashchau.work@gmail.com>
2026-09-11 14:49:50 +0530

    Create finding 3 digit even numbers.cpp

---
+++ DAILY QUES/finding 3 digit even numbers.cpp
@@ -0,0 +1,32 @@
+https://leetcode.com/problems/finding-3-digit-even-numbers/description/
+
+
+
+class Solution {
+public:
+    void solve(vector<int>& digits, vector<bool>& used, int num, int len, set<int>& st) {
+        if (len == 3) {
+            if (num % 2 == 0)
+                st.insert(num);
+            return;
+        }
+
+        for (int i = 0; i < digits.size(); i++) {
+            if (used[i]) continue;
+
+            // No leading zero
+            if (len == 0 && digits[i] == 0) continue;
+
+            used[i] = true;
+            solve(digits, used, num * 10 + digits[i], len + 1, st);
+            used[i] = false;
+        }
+    }
+
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        set<int> st;
+        vector<bool> used(digits.size(), false);
+        solve(digits, used, 0, 0, st);
+        return vector<int>(st.begin(), st.end());
+    }
+};
